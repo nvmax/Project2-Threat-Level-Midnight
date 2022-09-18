3002170cbd7e4b30cdf7fca9de4c40f06f8657e6
Garrett Lockhart <lockhartphotography1@gmail.com>
2022-09-17 22:08:10 -0600

    feat: added handlebars login and signup pages

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
-// const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-// const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
-
 
 // initializes the session store
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -14,8 +13,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const hbs = exphbs.create({ helpers });
-
+const hbs = exphbs.create({ helpers });
 
 // sets up the session and connects it to our Sequelize database
 const sess = {
@@ -24,20 +22,19 @@ const sess = {
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
-
 
 // tells the app to use the session
 app.use(session(sess));
 
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
