522438fea7806a13b5f44b27f0bc9beba6a25b40
SalTMongoose <acotterson@users.noreply.github.com>
2022-09-22 21:33:13 -0600

    working on pulling out  specific info

--- steam2.js
+++ steam2.js
@@ -1,9 +1,36 @@
-const id = 214630;
-// const id = 499450;
+const e = require("express");
+
+// const id = 214630;
+const id = 499450;
 // const id = 1184350;
 const url = `https://store.steampowered.com/api/appdetails?appids=${id}`;
 // const url = 'https://store.steampowered.com/api/appdetails?appids=10';
 // get json from steam api image.png http://store.steampowered.com/api/appdetails?appids=387990 log body
+
+let proc = [];
+let mem = [];
+let gpu = [];
+let directx = [];
+let storage = [];
+let os = [];
+
+
+function parseBetter(){
+  console.log(proc);
+  console.log(mem);
+  console.log(gpu);
+  console.log(directx);
+  console.log(storage);
+  console.log(os);
+
+  let gpuRecNv = gpu[1].match(/Geforce (\w+ \d+ )/i)
+  let gpuRecAMD = gpu[1].match(/Radeon (\w+ \d+)/i)
+
+  console.log(`Nvidia GPU: ${gpuRecNv[1]}`);
+  // console.log(`Nvidia Model #: ${gpuRecNv[2]}`);
+  console.log(`AMD GPU: ${gpuRecAMD[1]}`);
+  // console.log(`AMD Model #: ${gpuRecAMD[2]}`);
+}
 
 function parseNormSpecs(specs) {
   // console.log("there");
@@ -42,12 +69,15 @@ function parseNormSpecs(specs) {
     directx: false,
     storage: false,
   };
+
+
   // console.log(finalArray);
   // find key Processor and report value
   finalArray.forEach((item) => {
     if (item) {
       let key = Object.keys(item)[0];
       if (key === "Processor" && retrieved.processor === false) {
+        proc.push(item[key]);
         console.log(item[key]);
         retrieved.processor = true;
       }
@@ -58,6 +88,7 @@ function parseNormSpecs(specs) {
     if (item) {
       let key = Object.keys(item)[0];
       if (key === "Memory" && retrieved.memory === false) {
+        mem.push(item[key]);
         console.log(item[key]);
         retrieved.memory = true;
       }
@@ -71,6 +102,7 @@ function parseNormSpecs(specs) {
         (key === "Graphics" || key === "Video Card") &&
         retrieved.graphics === false
       ) {
+        gpu.push(item[key]);
         console.log(item[key]);
         retrieved.graphics = true;
       }
@@ -81,6 +113,7 @@ function parseNormSpecs(specs) {
     if (item) {
       let key = Object.keys(item)[0];
       if (key === "DirectX" && retrieved.directx === false) {
+        directx.push(item[key]);
         console.log(item[key]);
         retrieved.directx = true;
       }
@@ -94,6 +127,7 @@ function parseNormSpecs(specs) {
         (key === "Storage" || key === "Hard Disk Space") &&
         retrieved.storage === false
       ) {
+        storage.push(item[key]);
         console.log(item[key]);
         retrieved.storage = true;
       }
@@ -137,6 +171,14 @@ function parseSpecs(requirements) {
       });
       console.log(finalArray);
       finalArray.splice(0, 1);
+      proc.push(finalArray[0].Processor);
+      mem.push(finalArray[0].Memory);
+      gpu.push(finalArray[0].Graphics);
+      os.push(finalArray[0].OS);
+      proc.push(finalArray[1].Processor);
+      mem.push(finalArray[1].Memory);
+      gpu.push(finalArray[1].Graphics);
+      os.push(finalArray[1].OS);
       console.log("Minimum:");
       console.log(finalArray[0].Processor);
       console.log(finalArray[0].Memory);
@@ -160,48 +202,53 @@ function parseSpecs(requirements) {
   }
 }
 
-fetch(url)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    console.log(Object.keys(data)[0]);
-    if (data[Object.keys(data)[0]].data) {
-      const game = data[Object.keys(data)[0]].data;
-      console.log(data[Object.keys(data)[0]].data.type);
-      if (game) {
-        // game name
-        if (game.name) {
-          console.log(`Name: ${game.name}`);
-        }
-        // game price
-        if (game.free) {
-          console.log(`Free? ${game.free}`);
-        }
-        // how well it will run
-        // full description (any of them work)
-        if (game.detailed_description) {
-          console.log(`Description: ${game.detailed_description}`);
-        }
-        // release date
-        if (game.release_date) {
-          console.log(`Release Date: ${game.release_date.date}`);
-        }
-        // wallpaper (not raw)
-        if (game.background) {
-          console.log(`Wallpaper Link: ${game.background}`);
-        }
-        // which platforms it runs on (windows mac linux)
-        if (
-          typeof game.mac_requirements.recommended === "string" ||
-          typeof game.mac_requirements.minimum === "string"
-        ) {
-          console.log(`Runs on Mac`);
-          // console.log(typeof game.mac_requirements.minimum);
-          // requirements = game.mac_requirements;
-          // parseSpecs(requirements);
-          // }
+function main() {
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      console.log(Object.keys(data)[0]);
+      if (data[Object.keys(data)[0]].data) {
+        const game = data[Object.keys(data)[0]].data;
+        console.log(data[Object.keys(data)[0]].data.type);
+        if (game) {
+          // game name
+          if (game.name) {
+            console.log(`Name: ${game.name}`);
+          }
+
+          // game price
+          if (game.is_free) console.log(`Free? Yes!`);
+          else console.log("Free? No.");
+
+          // how well it will run
+          // full description (any of them work)
+          if (game.detailed_description) {
+            console.log(`Description: ${game.detailed_description}`);
+          }
+
+          // release date
+          if (game.release_date) {
+            console.log(`Release Date: ${game.release_date.date}`);
+          }
+
+          // wallpaper (not raw)
+          if (game.background) {
+            console.log(`Wallpaper Link: ${game.background}`);
+          }
+
+          // which platforms it runs on (windows mac linux)
+          if (
+            typeof game.mac_requirements.recommended === "string" ||
+            typeof game.mac_requirements.minimum === "string"
+          ) {
+            console.log(`Runs on Mac (Intel)`);
+            // console.log(typeof game.mac_requirements.minimum);
+            // requirements = game.mac_requirements;
+            // parseSpecs(requirements);
+          }
           if (
             typeof game.linux_requirements.recommended === "string" ||
             typeof game.linux_requirements.minimum === "string"
@@ -217,17 +264,19 @@ fetch(url)
           ) {
             requirements = game.pc_requirements;
             console.log(`Runs on PC`);
-            console.log(typeof game.pc_requirements);
             parseSpecs(requirements);
           }
         }
+        parseBetter();
       }
-    }
-  })
-  .catch(function (err) {
-    console.log(err);
-  });
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+    
+}
 
+main();
 // use steamspy.com/api.php?request=top100in2weeks
 
 //   const url1 = "https://steamspy.com/api.php?request=top100in2weeks";
