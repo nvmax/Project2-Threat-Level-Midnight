066bbe7b25e4998ce1cf90eef7f759dbca189557
unknown <nvmaxx@gmail.com>
2022-09-25 21:32:07 -0500

    fixed search on steamRoutes

--- controllers/api/steamRoutes.js
+++ controllers/api/steamRoutes.js
@@ -16,23 +16,21 @@ router.get('/', async (req, res) => {
 
 // get all games with a partial name match
 router.get('/:name', async (req, res) => {
-    if (req.params.name.includes(' ')) {
-        const nameArr = req.params.name.split(' ');
-        const name = nameArr.join('%');
+    if (req.params.name.match(/^\s*\d+\s*$/g)) {
         try {
             const steamData = await Steam.findAll({
                 where: {
-                    name: {
-                        [Op.like]: `%${name}%`
+                    appid: {
+                        [Op.like]: `${req.params.name.trim()}`
                     }
                 },
                 raw:true
             });
-            res.status(200).render('search', {game :steamData });
+            res.status(200).render('search', { game: steamData });
         } catch (err) {
             res.status(500).json(err);
         }
-    }  else if (req.params.name.includes('%')) {
+    }else if (req.params.name.includes('%')) {
         try {
             const steamData = await Steam.findAll({
                 where: {
@@ -47,20 +45,20 @@ router.get('/:name', async (req, res) => {
             res.status(500).json(err);
         }
     } else {
-        if (req.params.name.match(/\d+/g)) {
-            try {
-                const steamData = await Steam.findAll({
-                    where: {
-                        appid: {
-                            [Op.like]: `${req.params.name}`
-                        }
-                    },
-                    raw:true
-                });
-                res.status(200).render('search', { game: steamData });
-            } catch (err) {
-                res.status(500).json(err);
-            }
+        const nameArr = req.params.name.split(' ');
+        const name = nameArr.join('%');
+        try {
+            const steamData = await Steam.findAll({
+                where: {
+                    name: {
+                        [Op.like]: `%${name}%`
+                    }
+                },
+                raw:true
+            });
+            res.status(200).render('search', {game :steamData });
+        } catch (err) {
+            res.status(500).json(err);
         }
     }
     
