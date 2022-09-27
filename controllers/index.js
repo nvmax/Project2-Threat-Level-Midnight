d2e817c11058aea00dcce10feb4f7e8336007ed1
SalTMongoose <acotterson@users.noreply.github.com>
2022-09-27 09:24:43 -0600

    fixed index git push origin main

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const api = require('./api');
-const homeRoutes = require('./homeRoutes2.js');
+const homeRoutes = require('./homeRoutes.js');
 router.use('/api', api);
 router.use('/', homeRoutes)
 
