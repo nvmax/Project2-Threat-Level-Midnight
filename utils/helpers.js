daec55c272b387b965e17d3debfdd764e086d91f
SalTMongoose <acotterson@users.noreply.github.com>
2022-09-26 20:58:34 -0600

    comparison logic and some fixes

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,19 +1,19 @@
-const loggedIn = require('../utils/loggedIn');
-console.log(loggedIn);
+// const loggedIn = require('../utils/loggedIn');
+// console.log(loggedIn);
 
-const searchLink = document.getElementById('searchlink');
-const loginLink = document.getElementById('loginlink');
-const logout = document.getElementById('logout');
-const registerLink = document.getElementById('registerlink');
+// const searchLink = document.getElementById('searchlink');
+// const loginLink = document.getElementById('loginlink');
+// const logout = document.getElementById('logout');
+// const registerLink = document.getElementById('registerlink');
 
-if (loggedIn == "logged") {
-    searchLink.style.display = "inline-block";
-    logout.style.display = "inline-block";
-    loginLink.style.display = "none";
-    registerLink.style.display = "none";
-} else {
-    searchLink.style.display = "none";
-    logout.style.display = "none";
-    loginLink.style.display = "inline-block";
-    registerLink.style.display = "inline-block";
-}
+// if (loggedIn == "logged") {
+//     searchLink.style.display = "inline-block";
+//     logout.style.display = "inline-block";
+//     loginLink.style.display = "none";
+//     registerLink.style.display = "none";
+// } else {
+//     searchLink.style.display = "none";
+//     logout.style.display = "none";
+//     loginLink.style.display = "inline-block";
+//     registerLink.style.display = "inline-block";
+// }
