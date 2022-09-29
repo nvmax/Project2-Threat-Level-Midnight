d5e0627a5bc6809989d072377049f5ec9871758b
unknown <nvmaxx@gmail.com>
2022-09-28 21:21:06 -0500

    heroku fix

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 const hbs = exphbs.create({ helpers });
 
 // sets up the session and connects it to our Sequelize database
